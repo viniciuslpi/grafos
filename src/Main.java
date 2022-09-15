042e225385f285f58fd6fb782708254b62e690f7
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-09-15 13:46:09 -0300

    Refact dialog flow

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]){
-        Run.programPrimary();
-        Run.programSecondary();
+        showDialogs();
+    }
+
+    public static void showDialogs(){
+        int answer = showOptions();
+        do {
+            if(answer == 1) Run.programPrimary();
+            else Run.programSecondary();
+            answer = showOptions();
+        } while(answer != 0);
+    }
+
+    public static int showOptions() {
+        Scanner input = new Scanner(System.in);
+        System.out.println(">>> Matrizes ");
+        System.out.println("1 - Primeira Matriz\n2 - Segunda Matriz\n0 - Sair");
+        return input.nextInt();
     }
 }
 
--- src/Run.java
+++ src/Run.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class Run {
     public static void programPrimary() {
-        Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         Matriz matriz = new Matriz();
         int answer;
 
         System.out.println("Informe o tamanho da matriz (n x m): ");
-        int matrizLength = scan.nextInt();
-        matriz.matriz = new int[matrizLength][matrizLength];
+        int matrizLength = input.nextInt();
+        matriz.matriz    = new int[matrizLength][matrizLength];
 
         do {
             System.out.println("Digite a entrada do vértice: ");
-            int num1 = scan.nextInt();
+            int num1 = input.nextInt();
             System.out.println("Digite a saída do vértice: ");
-            int num2 = scan.nextInt();
+            int num2 = input.nextInt();
 
             matriz.insertValuePrimaryMatriz(num1, num2);
             matriz.show();
 
             System.out.println("\nDeseja continuar? (1 - Continuar | 0 - Encerrar)");
-            answer = scan.nextInt();
+            answer = input.nextInt();
         } while(answer == 1);
     }
 
@@ -30,7 +30,7 @@ public class Run {
         int answer;
 
         System.out.println("Informe o número de linhas da matriz: ");
-        int matrizLines    = scan.nextInt();
+        int matrizLines     = scan.nextInt();
         System.out.println("Informe o número de colunas da matriz: ");
         int matrizColumns   = scan.nextInt();
         matriz.matriz       = new int[matrizLines][matrizColumns];
